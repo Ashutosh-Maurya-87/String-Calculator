17ebf066e89c6d9cc865ea7f5abc7624b45e0c81
Ashutosh Maurya <ashumaurya486@gmail.com>
2024-09-27 21:54:33 +0530

    write function for negaive number and positive number strings to add them

--- stringCalc.js
+++ stringCalc.js
@@ -1,17 +1,23 @@
 console.log('code is written here')
 
 const addFunction = (value) => {
-    if(value === '') return 0
+    if (value === '') return 0
 
-    const splitStrings = value.split()
-    const arrayNum = splitStrings.map(num => parseInt(num))
-    console.log('arrayNum', arrayNum)
+    let arrayNum = []
+    const splitStrings = value.split(',')
+    splitStrings.map(num => arrayNum.push(parseInt(num)))
+    console.log('arrayNum', arrayNum, splitStrings)
 
     const negativeNumbers = arrayNum.filter(num => num < 0)
-    console.log('negative', negativeNumbers)
+    if (negativeNumbers.length > 0) {
+        throw new Error("Negative number is not allowed")
+    }
+
+    return arrayNum.reduce((acc,curr) => acc + curr, 0)
+console.log('negative', negativeNumbers)
 }
 console.log('empty string passes',addFunction('1,2,3'))
-console.log('empty string passes1',addFunction('1,-2,-3'))
+// console.log('empty string passes1', addFunction('1,-2,-3'))
 
 
 module.exports = addFunction
