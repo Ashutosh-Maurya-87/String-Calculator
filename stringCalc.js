80e90b1733c21c0c7bc6baa6974044cb0766432b
Ashutosh Maurya <ashumaurya486@gmail.com>
2024-09-27 21:46:00 +0530

    checking negative number

--- stringCalc.js
+++ stringCalc.js
@@ -3,8 +3,15 @@ console.log('code is written here')
 const addFunction = (value) => {
     if(value === '') return 0
 
+    const splitStrings = value.split()
+    const arrayNum = splitStrings.map(num => parseInt(num))
+    console.log('arrayNum', arrayNum)
+
+    const negativeNumbers = arrayNum.filter(num => num < 0)
+    console.log('negative', negativeNumbers)
 }
-console.log('epty string passes',addFunction(''))
+console.log('empty string passes',addFunction('1,2,3'))
+console.log('empty string passes1',addFunction('1,-2,-3'))
 
 
 module.exports = addFunction
