8763109054f79b32eabd3a7e37756eaa7f812270
Ashutosh Maurya <ashumaurya486@gmail.com>
2024-09-27 21:58:58 +0530

    test case for negative number

--- stringCalc.test.js
+++ stringCalc.test.js
@@ -6,5 +6,9 @@ describe('String Calculator', ()=> {
     test('empty string will return 0', () => {
         expect(stringCalcAddFunc('')).toBe(0)
     })
+
+    test('throw an error when negative number is passed as string',() => {
+        expect(() =>stringCalcAddFunc('1,-2,-3,-4')).toThrow('Negative number is not allowed')
+    })
 })
 console.log('empty test cases is passed')
