8db31aba8b276960f345a78239cf1366fc85c2a0
Ashutosh Maurya <ashumaurya486@gmail.com>
2024-09-27 21:32:54 +0530

    write a function addFun and test cases of that function

--- stringCalc.js
+++ stringCalc.js
@@ -1,3 +1,10 @@
 console.log('code is written here')
 
-// const test = 
+const addFunction = (value) => {
+    if(value === '') return 0
+
+}
+console.log('epty string passes',addFunction(''))
+
+
+module.exports = addFunction
--- stringCalc.test.js
+++ stringCalc.test.js
@@ -1,6 +1,10 @@
 console.log('test cases shoild be writtern here')
 
-const test = (string) => {
-    if(string === '') return 0
-}
-test('')
+const stringCalcAddFunc = require('./stringCalc')
+
+describe('String Calculator', ()=> {
+    test('empty string will return 0', () => {
+        expect(stringCalcAddFunc('')).toBe(0)
+    })
+})
+console.log('empty test cases is passed')
